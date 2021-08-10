c923a6cd0775094e1650a79905d767416fdb0674
Leo <leo@leons-Mac-mini.local>
2021-08-10 11:38:58 +0800

    v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,6 @@
 use async_trait::async_trait;
+use chrono::{DateTime, Utc};
 use std::{collections::HashMap, error, fmt, path::Path};
-
-#[derive(Debug)]
-pub struct SSSError(pub String);
-
-impl fmt::Display for SSSError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl error::Error for SSSError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        None
-    }
-}
 
 /// A trait encapsulating the operations required of LayerKV.
 #[async_trait]
@@ -24,9 +10,31 @@ pub trait SSSFacade: Sync + Send {
         &self,
         key: &str,
         file_path: &Path,
-        meta: Option<HashMap<&str, &str>>,
-    ) -> Result<bool, SSSError>;
-    async fn delete(&self, key: &str) -> Result<bool, SSSError>;
-    async fn download(&self, key: &str, save_path: &Path) -> Result<bool, SSSError>;
-    async fn head<'a>(&self, key: &str) -> Result<HashMap<&'a str, &'a str>, SSSError>;
+        meta: Option<HashMap<String, String>>,
+    ) -> Result<bool, SSSErr>;
+    async fn delete(&self, key: &str) -> Result<bool, SSSErr>;
+    async fn download(&self, key: &str, save_path: &Path) -> Result<bool, SSSErr>;
+    async fn head<'a>(&self, key: &str) -> Result<SSSHead, SSSErr>;
+}
+
+#[derive(Debug)]
+pub struct SSSHead {
+    pub content_length: i64,
+    pub e_tag: String,
+    pub last_modified: DateTime<Utc>,
+}
+
+#[derive(Debug)]
+pub struct SSSErr(pub String);
+
+impl fmt::Display for SSSErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl error::Error for SSSErr {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        None
+    }
 }
